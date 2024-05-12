f18c44cd92e2e6384c2ccb2650df05d825408406
Eduardo Garza <eduardo@garza.ca>
2024-05-12 09:57:07 -0700

    Added SMS to Node

--- src/api/routes/status/getRoot.js
+++ src/api/routes/status/getRoot.js
@@ -1,7 +1,14 @@
 import AppLogger from "../../../modules/LoggerModule/AppLogger.js";
+import SMSNotifications from "../../../modules/SMSNotificationsModule/SMSNotifications.js";
+
+const FILE_NAME = "[getRootHandler.js]";
 
 export default async function getRootHandler(req, res) {
-  AppLogger.info(`Start - Get Root Route Handler`);
+  AppLogger.info(`${FILE_NAME} - Start - Get Root Route Handler`);
+
+  const sender = new SMSNotifications();
+  await sender.sendSMS("Hello World");
+  AppLogger.info(`${FILE_NAME} - Message Sent`);
 
   return res.json({
     status: "success",
---
+++ src/modules/SMSNotificationsModule/SMSNotifications.js
@@ -0,0 +1,33 @@
+import twilio from "twilio";
+import AppLogger from "../LoggerModule/AppLogger.js";
+
+export default class SMSNotifications {
+  constructor() {
+    const twilioAccountSID = process.env.TWILIO_ACCOUNT_SID;
+    const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
+
+    if (!twilioAccountSID || !twilioAuthToken) {
+      errorMessage = `Twilio Environment Variables Not Set: TWILIO_ACCOUNT_SID: ${twilioAccountSID}, TWILIO_AUTH_TOKEN: ${twilioAuthToken}`;
+      AppLogger.error(errorMessage);
+      throw new Error(errorMessage);
+    }
+
+    this.client = twilio(
+      process.env.TWILIO_ACCOUNT_SID,
+      process.env.TWILIO_AUTH_TOKEN
+    );
+  }
+
+  async sendSMS(message) {
+    try {
+      const response = await this.client.messages.create({
+        body: message,
+        from: process.env.TWILIO_SENDER_PHONE_NUMBER,
+        to: process.env.TWILIO_RECEIVER_PHONE_NUMBER,
+      });
+      AppLogger.info(`SMS Sent: ${response.sid}`);
+    } catch (error) {
+      AppLogger.error(`SMS Error: ${error}`);
+    }
+  }
+}
